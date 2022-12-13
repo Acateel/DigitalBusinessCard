b68b8284e3843e5407e615cc5a43ac2be312e943
Taras Kozodoi <acateely@gmail.com>
2022-12-13 19:38:07 +0200

    add info content

--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -4,6 +4,11 @@ export default function Info(){
     return (
         <header>
             <img className="photo" src={photo} alt="photo" />
+            <h1 className="fullname">Taras Kozodoi</h1>
+            <h4 className="job">Frontend Developer</h4>
+            <h5 className="website">taraskozodoi.website</h5>
+            <button className="email">Email</button>
+            <button className="linkedln">Linkedln</button>
         </header>
     )
 }
