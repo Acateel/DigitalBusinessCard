46b855f7c6fc922740eed280546423d1a7659321
Taras Kozodoi <acateely@gmail.com>
2022-12-14 12:05:52 +0200

    add footer component with style

--- digital-business-card/src/components/Footer.jsx
+++ digital-business-card/src/components/Footer.jsx
@@ -1,3 +1,18 @@
+import './Footer.css'
+import facebookIco from "../../public/Facebook-Icon.png"
+import twitterIco from "../../public/Twitter-Icon.png"
+import instagramIco from "../../public/Instagram-Icon.png"
+import linkedinIco from "../../public/Linkedin-Icon.png"
+import githubIco from "../../public/GitHub-Icon.png"
+
 export default function Footer(){
-    return <h1>Footer</h1>
+    return (
+        <footer>
+            <img className="icon" src={twitterIco} alt="Twitter"/>
+            <img className="icon" src={facebookIco} alt="Facebook"/>
+            <img className="icon" src={instagramIco} alt="Instagram"/>
+            <img className="icon" src={linkedinIco} alt="Linkedin"/>
+            <img className="icon" src={githubIco} alt="GitHub"/>
+        </footer>
+    )
 }
