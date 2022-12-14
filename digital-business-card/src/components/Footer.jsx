1f45fc9f2c4ac86fd6d9fe89b1de4e949fa8970d
Taras Kozodoi <acateely@gmail.com>
2022-12-14 12:30:03 +0200

    add links

--- digital-business-card/src/components/Footer.jsx
+++ digital-business-card/src/components/Footer.jsx
@@ -8,11 +8,21 @@ import githubIco from "../../public/GitHub-Icon.png"
 export default function Footer(){
     return (
         <footer>
-            <img className="icon" src={twitterIco} alt="Twitter"/>
+            <a href='https://twitter.com/'>
+                <img className="icon" src={twitterIco} alt="Twitter"/>
+            </a>
+            <a href='https://www.facebook.com/profile.php?id=100009216018247'>
             <img className="icon" src={facebookIco} alt="Facebook"/>
+            </a>
+            <a href='https://www.instagram.com/akateel/'>
             <img className="icon" src={instagramIco} alt="Instagram"/>
-            <img className="icon" src={linkedinIco} alt="Linkedin"/>
-            <img className="icon" src={githubIco} alt="GitHub"/>
+            </a>
+            <a href='https://www.linkedin.com/'>
+                <img className="icon" src={linkedinIco} alt="Linkedin"/>
+            </a>
+            <a href='https://github.com/Acateel'>
+                <img className="icon" src={githubIco} alt="GitHub"/>
+            </a>
         </footer>
     )
 }
