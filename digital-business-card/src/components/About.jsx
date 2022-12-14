605a8b2490fe0d5382017e47cf592a5cf49d069a
Taras Kozodoi <acateely@gmail.com>
2022-12-14 11:31:33 +0200

    add about commponent

--- digital-business-card/src/components/About.jsx
+++ digital-business-card/src/components/About.jsx
@@ -1,3 +1,12 @@
+
 export default function About(){
-    return <h1>About</h1>
+    return (
+        <main>
+            <h1>About</h1>
+            <p className="text">
+                I am a frontend developer with a particular interest in making things that look awesome. 
+                I try to keep up with design and best practices, and am always looking for new things to learn.
+            </p>
+        </main>
+    )
 }
