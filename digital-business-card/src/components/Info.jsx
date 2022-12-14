da3c48d493e079c79d26d96e4db4335765f3196e
Taras Kozodoi <acateely@gmail.com>
2022-12-14 12:21:09 +0200

    add mail

--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -9,7 +9,9 @@ export default function Info(){
             <h1 className="fullname">Taras Kozodoi</h1>
             <h4 className="job">Frontend Developer</h4>
             <h5 className="website">taraskozodoi.website</h5>
-            <button className="email"><img src={emailIco}/><span>Email</span></button>
+            <a href='mailto:acateely@gmail.com'>
+                <button className="email"><img src={emailIco}/><span>Email</span></button>
+            </a>
         </header>
     )
 }
