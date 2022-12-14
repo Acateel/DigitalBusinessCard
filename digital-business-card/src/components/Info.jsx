357ec9a015e1446859fe3187c326dfc480e0393b
Taras Kozodoi <acateely@gmail.com>
2022-12-14 10:34:57 +0200

    add css to email button, finish info component

--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -1,4 +1,5 @@
 import photo from "../../public/temporary-image.jpg"
+import emailIco from "../../public/Mail.png"
 
 export default function Info(){
     return (
@@ -7,7 +8,7 @@ export default function Info(){
             <h1 className="fullname">Taras Kozodoi</h1>
             <h4 className="job">Frontend Developer</h4>
             <h5 className="website">taraskozodoi.website</h5>
-            <button className="email">Email</button>
+            <button className="email"><img src={emailIco}/><span>Email</span></button>
         </header>
     )
 }
